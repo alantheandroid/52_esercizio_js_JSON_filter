ffc1564adffe787f0ca1858a4cb665e854229b1e
alantheandroid <alessio.ilardi@gmail.com>
2022-06-17 20:27:46 +0200

    feat: add JSON filter for numerical values

--- exercise-4.js
+++ exercise-4.js
@@ -1,10 +1,12 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
+  firstName: "Mario",
+  lastName: "Rossi",
   age: 25,
 };
 
-const json = JSON.stringify(person);
+const json = JSON.stringify(
+  Object.entries(person).filter(([key, value]) => typeof value == "number")
+);
 
 console.log(json); // Should return: { id: 1, age: 25 }
